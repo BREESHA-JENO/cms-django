443a508cbd11efd2d3ce6141319eb051501e330c
liviya7 <oliviya5113@gmai.com>
2025-10-24 14:32:38 +0530

    modified settings.py

--- cms_backend_api/cms_backend_api/settings.py
+++ cms_backend_api/cms_backend_api/settings.py
@@ -159,7 +159,7 @@ AUTH_USER_MODEL = 'Authentication.User'
 
 # SIMPLE_JWT = {
 #     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-#     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+#     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1)mm,
 #     'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
 #     'SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER': timedelta(days=1),
 #     'SLIDING_TOKEN_LIFETIME_LATE_USER': timedelta(days=30),
